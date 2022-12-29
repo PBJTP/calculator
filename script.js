0a3407a731d46e22d74ace0e485896735f814470
PBJTP <jplypick1990@gmail.com>
2022-12-29 11:00:50 -0800

    Add basic functions

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('hello world');
+
+function add (x,y) {
+    return x + y;
+};
+
+function subtract(x,y) {
+    return x - y;
+};
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    if (operator == '/') {
+        return divide(x,y);
+    } else if (operator == '*') {
+        return multiply(x,y);
+    } else if (operator == '+') {
+        return add(x,y);
+    } else if (operator == '-') {
+        return subtract(x,y);
+    } else {
+        return 'ERROR';
+    }
+}
