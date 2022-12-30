b550c9497fab990a7cd35af909e59a10754e41a8
PBJTP <jplypick1990@gmail.com>
2022-12-29 22:49:57 -0800

    Add operator button functionality and variable storage

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('hello world');
-
 let data = { //fancy data storage
     num1:0,
     num2:0,
@@ -7,11 +5,13 @@ let data = { //fancy data storage
 }
 
 let buttons = document.querySelectorAll('.numberButton'); //select number buttons
+let decimal = document.querySelector('.decimalButton');
 let clearBtn = document.querySelector('.clearButton'); // All clear button
 let backspace = document.querySelector('.deleteButton'); //Delete button
 let display = document.getElementById('display'); //display window
+let operators = document.querySelectorAll('.opButton');
 
-display.maxLength = '8';
+display.maxLength = '8'; //DOESNT WORK
 
 buttons.forEach(function(button) { // add listeners to numbers only
     button.addEventListener('click', function() {
@@ -23,11 +23,31 @@ buttons.forEach(function(button) { // add listeners to numbers only
     });
 });
 
+operators.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        data.operator = '';
+        data.operator = this.value;
+        data.num1 = Number(display.innerHTML);
+        display.innerHTML = '0';
+
+
+        console.log(data.num1);
+        console.log(data.operator);
+    })
+})
+
 clearBtn.addEventListener('click', function() { //All Clear
     display.innerHTML = 0;
 });
 
-backspace.addEventListener('click', function() {
+// decimal.addEventListener('click', function() {   !!!!!!!!!PROJECT FOR LATER!!!!!!!!!
+//     let array = display.innerHTML.split();
+//     array.unshift('.');
+//     let newValue = array.join('');
+//     display.innerHTML = newValue;
+// })
+
+backspace.addEventListener('click', function() { //backspace button
     let array = display.innerHTML.split('');
     array.pop();
     let newValue = array.join('');
@@ -53,17 +73,18 @@ function divide(x,y) { //division operator
     return x / y;
 }
 
-function operate(operator, x, y) { // EQUALS BUTTON PROGRAM
-    if (operator == '/') {
+function operate(operators, x, y) { // EQUALS BUTTON PROGRAM
+    if (operators == '/') {
         return divide(x,y);
-    } else if (operator == '*') {
+    } else if (operators == '*') {
         return multiply(x,y);
-    } else if (operator == '+') {
+    } else if (operators == '+') {
         return add(x,y);
-    } else if (operator == '-') {
+    } else if (operators == '-') {
         return subtract(x,y);
     } else {
         return 'ERROR';
     }
+    data.operator = '';
 };
 
